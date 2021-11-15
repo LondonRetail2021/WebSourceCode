df5c867ba416bb794786bfb1924cba0bde48ca65
root <root@ip-172-31-31-76.eu-west-2.compute.internal>
2021-11-15 20:52:48 +0000

    added dockerfile

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var reload = require('reload');
 /**
  * For set port or default 7000 posr.
  */
-app.set('port', process.env.POST || 7000);
+app.set('port', process.env.POST || 7001);
 /**
  * Set view engine & point a view folder.
  */
@@ -67,7 +67,7 @@ app.use(require('./routers/pages'));
 /**
  * Create server.
  */
-var server = app.listen(7000, function() {
+var server = app.listen(7001, function() {
   console.log('Running server');
 })
 /**
